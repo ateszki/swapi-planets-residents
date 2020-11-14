5b48e855ed5b6eb8a191f7b8c13878beb43d7437
ateszki <ateszki@gmail.com>
2020-11-14 09:51:30 -0300

    Minimal testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
+
+
+test('renders planets and residents', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getAllByText(/Planets and Residents/i);
+  expect(linkElement).toHaveLength(2);
 });
+
+
+it('renders App without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+
